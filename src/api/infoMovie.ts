ac94fc9b2fe20373dc8e1e154c8a449238abd39f
Cleudeir <Cleudeirsilva@gmail.com>
2023-01-05 20:53:07 -0300

    update

--- src/api/infoMovie.ts
+++ src/api/infoMovie.ts
@@ -8,8 +8,8 @@ async function getMovie(_url: string): Promise<string | null> {
   if (doc === null || doc === undefined) {
     return null
   }
-  const response : string | undefined = doc.querySelectorAll('iframe[name="Player"]')[0].attributes.src.textContent
-  if (response === null || response === undefined) {
+  const response : any | string | undefined = doc.querySelectorAll('iframe[name="Player"]')[0]?.attributes.src.textContent
+  if (response?.length> 0 || response === null || response === undefined) {
     return null
   }
   const [one, two] = response.split(".php");
@@ -48,7 +48,6 @@ async function getIMDB(item: DataMovie): Promise<DataMovie | null> {
 
 export default async function infoMovie(item: DataMovie): Promise<DataMovie | null> {
   const urlBase = "https://redecanais.la"+ item.url;
-  console.log(urlBase)
     const url: string | null = await cache(urlBase,getMovie);
     if (url === null) {return url}
     const itemMovie = {...item, url}
--- src/index.ts
+++ src/index.ts
@@ -20,21 +20,25 @@ server.get('/', async (_req: Req, _res: Resp): Promise<void> => {
 // movie
 server.get('/api/mapMovie', async (_req: Req, _res: Resp): Promise<void> => {
     const data = await mapMovie()
+    console.log('mapMovie')
     _res.status(200).json(data)
 })
 server.post('/api/infoMovie', async (_req: Req, _res: Resp): Promise<void> => {
     const item = _req.body.item as DataMovie
     const data = await infoMovie(item)
+    console.log('infoMovie ',item.title)
     _res.status(200).json(data)
 })
 
 // tv
 server.get('/api/mapTv', async (_req: Req, _res: Resp): Promise<void> => {
     const data = await mapTv()
+    console.log('mapTv')
     _res.status(200).json(data)
 })
 server.post('/api/infoTv', async (_req: Req, _res: Resp): Promise<void> => {
     const item = _req.body.item as DataTv
     const data = await infoTv(item)
+    console.log('infoTv ',item.title)
     _res.status(200).json(data)
 })
