00ff5b06262e8637abe238c8d3d413b495b6d211
Cleudeir <cleudeirsilva@gmail.com>
2023-01-13 00:11:55 -0300

    update

--- src/api/infoTvList.ts
+++ src/api/infoTvList.ts
@@ -33,12 +33,40 @@ async function geTv(url: string): Promise<episode[] | null> {
   }
   return episodes;
 }
+async function getTmdbID(item: DataTv): Promise<DataTv | null> {
+  try {
+    const id = item.id
+    const pullInfo = await fetch(
+      `https://api.themoviedb.org/3/tv/${id}?api_key=5417af578f487448df0d4932bc0cc1a5&language=pt-BR`
+    )
+    const jsonInfo = await pullInfo.json()
+    if (jsonInfo) {
+      const obj = {
+        ...item,
+        ...jsonInfo,
+        backdrop_path: jsonInfo.backdrop_path
+          ? 'https://image.tmdb.org/t/p/original/' +
+          jsonInfo.backdrop_path
+          : null,
+        poster_path: jsonInfo.poster_path
+          ? 'https://image.tmdb.org/t/p/w342' + jsonInfo.poster_path
+          : null,
+      }
+      return obj
+    } else {
+      return null
+    }
+  } catch (error) {
+    return null
+  }
+}
 
 export default async function infoTvList(item: DataTv): Promise<DataTv | null> {
   const url = "https://redecanais.la/" + item.url + '.html';
   console.log('url: ', url);
   const episodes = await cache(url, geTv);
+  const infos = await getTmdbID(item)
   if (episodes === null) { return null }
-  const data = { ...item, episodes }
+  const data = { ...infos, episodes }
   return data
 }
