3a7d88470638060800208ea638fe78c93dd56eb5
Cleudeir <Cleudeirsilva@gmail.com>
2023-02-01 12:38:47 -0300

    up

--- src/components/asyncCrawler.ts
+++ src/components/asyncCrawler.ts
@@ -5,8 +5,8 @@ const getWebsiteContent = async (url: string): Promise<string | undefined> => {
   try {
     const option = {
       headers: {
-        "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        "Accept-Language": 'en',
+        "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        "Accept-Language": 'en-US,en;q=0.9,pt;q=0.8,zh-CN;q=0.7,zh-TW;q=0.6,zh;q=0.5,ko;q=0.4',
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
       }
     }
