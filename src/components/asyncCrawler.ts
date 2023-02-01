2e6778c85f7aa61a40222bcbf2b4d45e1b939b6b
Cleudeir <Cleudeirsilva@gmail.com>
2023-02-01 12:20:43 -0300

    up

--- src/components/asyncCrawler.ts
+++ src/components/asyncCrawler.ts
@@ -3,7 +3,14 @@ import fetch from "node-fetch";
 
 const getWebsiteContent = async (url: string): Promise<string | undefined> => {
   try {
-    const content = await fetch(url);
+    const option = {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0",
+        "Accept": "application/json"
+      }
+    }
+    const content = await fetch(url, option);
+
     console.log('url: ', url);
     console.log('content: ', content);
     const text = await content.text();
