b1d3e2aa9d894f307ccde9300ea80513225f562d
Cleudeir <Cleudeirsilva@gmail.com>
2023-02-02 08:25:14 -0300

    up

--- src/components/asyncCrawler.ts
+++ src/components/asyncCrawler.ts
@@ -1,11 +1,33 @@
 import { JSDOM } from "jsdom";
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+import cloudscraper from 'cloudscraper';
 
 const getWebsiteContent = async (url: string): Promise<string | undefined> => {
   try {
-    const content = await fetch(url);
-    const text = await content.text();
-    return text;
+    const content = await cloudscraper({
+      uri: url,
+      headers: {
+        // User agent, Cache Control and Accept headers are required
+        // User agent is populated by a random UA.
+        'User-Agent': 'Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36',
+        'Cache-Control': 'private',
+        'Accept': 'application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5'
+      },
+      // Cloudscraper automatically parses out timeout required by Cloudflare.
+      // Override cloudflareTimeout to adjust it.
+      cloudflareTimeout: 5000,
+      // Reduce Cloudflare's timeout to cloudflareMaxTimeout if it is excessive
+      cloudflareMaxTimeout: 30000,
+      // followAllRedirects - follow non-GET HTTP 3xx responses as redirects
+      followAllRedirects: true,
+      // Support only this max challenges in row. If CF returns more, throw an error
+      challengesToSolve: 3,
+      // Remove Cloudflare's email protection, replace encoded email with decoded versions
+      decodeEmails: false,
+      // Support gzip encoded responses (Should be enabled unless using custom headers)
+      gzip: true,
+    });
+    // const text = await content.text();
+    return content;
   } catch (error) {
     console.log(error);
   }
