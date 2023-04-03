485f651e4c69801b6315b813eb279453b4084ef4
Cleudeir <cleudeirsilva@gmail.com>
2023-04-03 10:54:40 -0300

    cache time

--- src/components/cache.ts
+++ src/components/cache.ts
@@ -20,18 +20,18 @@ async function cache(url: string, _function: (url: string) => Promise<any>): Pro
     const read: any = await fs.readFile(`temp/${name}.json`);
     console.log('name: ', name);
     let {data, time}: any = await JSON.parse(read);
-    if (!data || data.length === 0 || !time || timeNow-time > 24*60*60*1000) {
+    if (!data || data.length === 0 || !time || (Number(timeNow) - Number(time)) > 24*60*60*1000) {
       data = await _function(url)
       console.log('>>>>>>>>>: data search', data.length);
       if (data) {
-        await fs.writeFile(`temp/${name}.json`, JSON.stringify({data,time: timeNow}));
+        void fs.writeFile(`temp/${name}.json`, JSON.stringify({data,time: timeNow}));
         return data
       }
     }
     return data
   } catch (error) {
     const data = await _function(url)
-    await fs.writeFile(`temp/${name}.json`, JSON.stringify({data,time: timeNow}))
+    void fs.writeFile(`temp/${name}.json`, JSON.stringify({data,time: timeNow}))
     return data
   }
 }
