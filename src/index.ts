7849b97a76aca1e075f775165c3fb8bf56afc28f
Cleudeir <cleudeirSilva@gmail.com>
2023-05-22 10:44:43 -0300

    up

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import mapTv from './api/map/tv';
 import server from './class/Server';
 import express from 'express';
 import { DataMovie, DataTv, categoryProps, popularProps } from './components/interfaces';
-import Delete from './api/Delete';
 import infoTv from './api/info/tv'
 import fs from 'fs';
 import category from './api/tmdb/category';
@@ -29,17 +28,14 @@ server.get('/', async (_req: Req, _res: Resp): Promise<void> => {
 })
 
 // movie
-const deleteMovie = new Delete('ExcludesMovie')
 
 // create a function receive http request Header and identify who make this request
 server.get('/map/movie', async (_req: Req, _res: Resp): Promise<void> => {
     const time = Date.now()
     const data = await mapMovie()
     console.log('/map/movie')
-    const toRemove = await deleteMovie.read() as any[]
-    const dataFilter = data?.filter((item: any): any => !toRemove.includes(item.title))
     console.log('time: ', (Date.now() - time) / 1000, 's')
-    _res.status(200).json(dataFilter)
+    _res.status(200).json(data)
 })
 
 server.post('/info/movie', async (_req: Req, _res: Resp): Promise<void> => {
@@ -67,28 +63,14 @@ server.post('/tmdb/popular', async (_req: Req, _res: Resp): Promise<void> => {
     _res.status(200).json(data)
 })
 
-server.delete('/delete/movie', async (_req: Req, _res: Resp): Promise<void> => {
-    const time = Date.now()
-    const item = _req.body as unknown as DataMovie
-    console.log('/delete/movie', item.title)
-    const data = await deleteMovie.insert(item)
-    console.log('time: ', (Date.now() - time) / 1000, 's')
-    _res.status(200).json(data)
-})
-
 // tv
-const deleteTv = new Delete('ExcludesTv')
 
 server.get('/map/tv', async (_req: Req, _res: Resp): Promise<void> => {
     const time = Date.now()
     const data = await mapTv()
     console.log('/map/tv')
-    const toRemove = await deleteTv.read() as any[]
-    const dataFilter = data.filter((item): any => {
-        return !toRemove.includes(item.title);
-    })
     console.log('time: ', (Date.now() - time) / 1000, 's')
-    _res.status(200).json(dataFilter)
+    _res.status(200).json(data)
 })
 
 server.post('/list/tv', async (_req: Req, _res: Resp): Promise<void> => {
@@ -105,15 +87,6 @@ server.post('/info/tv', async (_req: Req, _res: Resp): Promise<void> => {
     const item = _req.body as unknown as DataTv
     console.log('/info/tv', item);
     const data = await infoTv(item)
-    console.log('time: ', (Date.now() - time) / 1000, 's')
-    _res.status(200).json(data)
-})
-
-server.delete('/delete/tv', async (_req: Req, _res: Resp): Promise<void> => {
-    const time = Date.now()
-    const item = _req.body as unknown as DataMovie
-    console.log('/delete/tv', item.title)
-    const data = await deleteTv.insert(item)
     console.log('time: ', (Date.now() - time) / 1000, 's')
     _res.status(200).json(data)
 })
